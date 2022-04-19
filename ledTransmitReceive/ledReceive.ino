6c11a937a320e3b48abf146c4db806f2e563dea6
Dean Meyer <72794590+dean-meyer@users.noreply.github.com>
2022-04-19 12:48:00 -0400

    Update ledReceive.ino

--- ledTransmitReceive/ledReceive.ino
+++ ledTransmitReceive/ledReceive.ino
@@ -5,13 +5,13 @@ char data = '0';       //data variable set to 0 initially
 void setup() 
 {
   Serial.begin(9600);        //start serial communication
-  pinMode(LED, OUTPUT);      //set pinMode for LED
+  pinMode(LED, OUTPUT);      //set pinMode for LED as output
   digitalWrite(LED, LOW);    //set initial state of LED off
 }
 
 void loop() 
 {
-  if (Serial.available() > 0)   //if statement to beging looking for data from transmitter
+  if (Serial.available() > 0)   //if statement to begin looking for data from transmitter
   {
     data = Serial.read();       //read data from serial comm to the data variable
 
