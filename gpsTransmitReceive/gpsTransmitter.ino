f7ba8d726be88d0a454b88fc77b90a59db2f3016
Dean Meyer <72794590+dean-meyer@users.noreply.github.com>
2022-04-19 12:25:10 -0400

    Update gpsTransmitter.ino

--- gpsTransmitReceive/gpsTransmitter.ino
+++ gpsTransmitReceive/gpsTransmitter.ino
@@ -1,16 +1,19 @@
-//This code drew heavily on the lessons provided by Paul McWhorter from toptechboy.com
+/*This code drew from lessons provided by Paul McWhorter. It uses the Adafruit GPS library to read data from 
+NMEA GPS strings using an Adafruit GPS module, parse them, and then output the required data for transmission 
+over XBee from a remote circuit with XBee and GPS modules to a local circuitk with an XBee module for data display
+*/
 
 #include <Adafruit_GPS.h>      //install the adafruit GPS library
 #include <SoftwareSerial.h>    //load the software serial library
-SoftwareSerial mySerial(8,7);  //initialize the software serial port on Arduino pins 7 & 8
+SoftwareSerial mySerial(8,7);  //initialize the software serial port on Arduino pins 7 & 8 for the GPS module
 Adafruit_GPS GPS(&mySerial);   //create the GPS object
  
-String NMEA1;   //variable for first NMEA sentence
-String NMEA2;   //variable for second NMEA sentence
-char c;         //read characters coming from the GPS
-float deg;      //will hold position data in simple degree format
-float degWhole; //variable for position data
-float degDec;   //variable for position data
+String NMEA1;    //variable for first NMEA sentence
+String NMEA2;    //variable for second NMEA sentence
+char c;          //read characters coming from the GPS
+float deg;       //will hold position data in simple degree format
+float degWhole;  //variable for position data
+float degDec;    //variable for position data
  
  
 void setup() {
@@ -36,7 +39,7 @@ void loop() {
   Serial.print(GPS.year, DEC);
   Serial.print(", ");
 
-    //print the current time
+  //print the current time
   Serial.print("Time (GMT): ");
   if (GPS.hour < 10) { 
       Serial.print('0'); 
@@ -63,7 +66,7 @@ void loop() {
   if (GPS.lat=='S') {  
     deg= (-1)*deg;
   }
-  //print longitude data
+  //print latitude data
   Serial.print(deg,4); 
   Serial.print(","); 
 
@@ -76,19 +79,19 @@ void loop() {
   if (GPS.lon=='W') {  
     deg= (-1)*deg;
   }
-  //print latitude data
+  //print longitude data
   Serial.print(deg,4);
   Serial.println(",");
 
   //print other data
   Serial.print("Altitude: ");
-  Serial.print(GPS.altitude);        //alititude
+  Serial.print(GPS.altitude);        //alititude, in meters
   Serial.print(" m, ");
   Serial.print("Angle: ");
-  Serial.print(GPS.angle);           //bearing
+  Serial.print(GPS.angle);           //heading, in degrees
   Serial.print("°, ");
   Serial.print("Speed: "); 
-  Serial.print(GPS.speed);           //speed (in knots)
+  Serial.print(GPS.speed);           //speed, in knots
   Serial.print(" kn, ");
   Serial.print("Satellite Count: ");
   Serial.println(GPS.satellites);    //# of satellites seen by GPS
@@ -99,19 +102,23 @@ void loop() {
 void readGPS() {
   
   clearGPS();
-  while(!GPS.newNMEAreceived()) { //loop until you have a good NMEA sentence
+  //loop until you have a good NMEA sentence
+  while(!GPS.newNMEAreceived()) { 
     c=GPS.read();
   }
-  GPS.parse(GPS.lastNMEA()); //parse the good NMEA sentence
+  //parse the good NMEA sentence 
+  GPS.parse(GPS.lastNMEA()); 
   NMEA1=GPS.lastNMEA();
   
-   while(!GPS.newNMEAreceived()) { //loop until you have a good NMEA sentence
-    c=GPS.read();
+  //loop until you have a good NMEA sentence
+  while(!GPS.newNMEAreceived()) { 
+   c=GPS.read();
   }
-  GPS.parse(GPS.lastNMEA()); //parse the good NMEA sentence
+  //parse the good NMEA sentence
+  GPS.parse(GPS.lastNMEA()); 
   NMEA2=GPS.lastNMEA();
 
-  //uncomment the next 3 lines to print the NMEA sentences for GGA and RMC if desired
+  //uncomment the next 3 lines to print the raw NMEA sentences for GGA and RMC if desired
   //Serial.println(NMEA1);
   //Serial.println(NMEA2);
   //Serial.println("");
@@ -120,18 +127,25 @@ void readGPS() {
 
 //function to clear bad GPS data from buffer, three loops seems to work best
 void clearGPS() {  
-  while(!GPS.newNMEAreceived()) { //loop until you have a good NMEA sentence
+  //loop until you have a good NMEA sentence
+  while(!GPS.newNMEAreceived()) { 
     c=GPS.read();
   }
-  GPS.parse(GPS.lastNMEA()); //parse the good NMEA sentence
+  //parse the good NMEA sentence
+  GPS.parse(GPS.lastNMEA()); 
   
-  while(!GPS.newNMEAreceived()) { //loop until you have a good NMEA sentence
+  //loop until you have a good NMEA sentence
+  while(!GPS.newNMEAreceived()) { 
     c=GPS.read();
   }
-  GPS.parse(GPS.lastNMEA()); //parse the good NMEA sentence
-   while(!GPS.newNMEAreceived()) { //loop until you have a good NMEA sentence
-    c=GPS.read();
+  //parse the good NMEA sentence
+  GPS.parse(GPS.lastNMEA()); 
+ 
+ //loop until you have a good NMEA sentence
+  while(!GPS.newNMEAreceived()) { 
+   c=GPS.read();
   }
-  GPS.parse(GPS.lastNMEA()); //parse the good NMEA sentence
+  //parse the good NMEA sentence
+  GPS.parse(GPS.lastNMEA()); 
   
 }
